d88bd09b38338984dabe0846a282bbe9f1a80fc2
Philippe Fonzin <pfonzin@gmail.com>
2020-06-07 19:06:52 -0400

    Add letter tile possibilities problem

---
+++ misc/letter_tile_possibilities.js
@@ -0,0 +1,19 @@
+// You have a set of tiles, where each tile has one letter tiles[i] printed on it.
+// Return the number of possible non - empty sequences of letters you can make.
+
+/**
+ * @param {string} tiles
+ * @return {number}
+ */
+var numTilePossibilities = function (tiles) {
+
+};
+
+// Input: "AAB"
+// Output: 8
+// Explanation: The possible sequences are "A", "B", "AA", "AB", "BA", "AAB", "ABA", "BAA".
+
+// Note:
+
+// 1 <= tiles.length <= 7
+// tiles consists of uppercase English letters.
