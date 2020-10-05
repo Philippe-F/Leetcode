9a9e8ea6a4f23311278eb5f0b914b6e2dfaa0baa
Philippe Fonzin <pfonzin@gmail.com>
2020-10-05 12:39:09 -0400

    solve shuffle string

---
+++ medium_problems/shuffle_string.js
@@ -0,0 +1,16 @@
+// Given a string s and an integer array indices of the same length.
+
+// The string s will be shuffled such that the character at the ith position 
+// moves to indices[i] in the shuffled string.
+
+// Return the shuffled string.
+
+var restoreString = function (s, indices) {
+  let newStr = []
+  for (let i = 0; i < indices.length; i++) {
+    let idx = indices[i]
+    newStr[idx] = s[i]
+  }
+
+  return newStr.join('')
+};
