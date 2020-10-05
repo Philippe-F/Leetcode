1e31d5154bf830aa7959cbc0a038de181bb1897c
Philippe Fonzin <pfonzin@gmail.com>
2020-10-05 13:08:14 -0400

    solve shuffle array

--- medium_problems/shuffle_array.js
+++ medium_problems/shuffle_array.js
@@ -1,8 +1,15 @@
+// Given the array nums consisting of 2n elements in the 
+// form[x1, x2,..., xn, y1, y2,..., yn].
+
+// Return the array in the form[x1, y1, x2, y2,..., xn, yn].
+
 var shuffle = function (nums, n) {
-  let result = [];
-  for (let i = 0; i < n; i++) {
-    result.push(nums[i]);
-    result.push(nums[i + n]);
+  let newNums = [], i = 0
+  while (newNums.length < nums.length) {
+    newNums.push(nums[i])
+    newNums.push(nums[i + n])
+    i++
   }
-  return result;
+
+  return newNums
 };
