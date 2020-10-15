c9ca66a3cdf9db9c10c5bf75cf30c8921c11d507
Philippe Fonzin <pfonzin@gmail.com>
2020-10-15 19:48:56 -0400

    solve cherry pickup III

---
+++ medium_problems/cherry_pickup_ii.js
@@ -0,0 +1,6 @@
+// Given a rows x cols matrix grid representing a field of cherries.Each cell in 
+// grid represents the number of cherries that you can collect.
+
+// You have two robots that can collect cherries for you, 
+// Robot #1 is located at the top - left corner(0, 0), and Robot #2 is 
+// located at the top - right corner(0, cols - 1) of the grid.
