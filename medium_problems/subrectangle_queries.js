c686d36bc3cf3bb0bd173608765cf6b9bce66595
Philippe Fonzin <pfonzin@gmail.com>
2020-11-03 21:18:43 -0500

    subrectangle queries

--- medium_problems/subrectangle_queries.js
+++ medium_problems/subrectangle_queries.js
@@ -14,17 +14,5 @@
  * @param {number[][]} rectangle
  */
 var SubrectangleQueries = function (rectangle) {
-  this.rectangle = rectangle;
-};
 
-SubrectangleQueries.prototype.updateSubrectangle = function (row1, col1, row2, col2, newValue) {
-  for (let row = row1; row <= row2; row += 1) {
-    for (let col = col1; col <= col2; col += 1) {
-      this.rectangle[row][col] = newValue;
-    }
-  }
-};
-
-SubrectangleQueries.prototype.getValue = function (row, col) {
-  return this.rectangle[row][col];
 };
