608bbe5e04b5c065c9b55de5a3808ce5c17b4e95
Philippe Fonzin <pfonzin@gmail.com>
2020-11-03 21:17:47 -0500

    xor Operation

--- medium_problems/xor_operation.js
+++ medium_problems/xor_operation.js
@@ -1,11 +1,3 @@
 var xorOperation = function (n, start) {
-  let arr = [], xor
-  for (let i = 0; i < n; i++) {
-    arr.push(start + (i * 2))
-  }
-  xor = arr[0]
-  for (let i = 1; i < n; i++) {
-    xor = xor ^ arr[i]
-  }
-  return xor
+
 };
