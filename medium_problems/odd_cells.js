b98d236026ce6022c8dc7910d89bd49a44bd8cf0
Philippe Fonzin <pfonzin@gmail.com>
2020-11-23 21:44:13 -0500

    Odd cells

--- medium_problems/odd_cells.js
+++ medium_problems/odd_cells.js
@@ -1,3 +1,11 @@
+
+// Given n and m which are the dimensions of a matrix initialized by zeros and 
+// given an array indices where indices[i] = [ri, ci].For each pair of[ri, ci] 
+// you have to increment all cells in row ri and column ci by 1.
+
+// Return the number of cells with odd values in the matrix after applying the 
+// increment to all indices.
+
 var oddCells = function (n, m, indices, odd = 0) {
   let matrix = [...Array(n)].map(() => Array(m).fill(0));
 
