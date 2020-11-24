b3faded7b366f5e9916fa32072dee0838a48e551
Philippe Fonzin <pfonzin@gmail.com>
2020-11-23 21:38:24 -0500

    K waakest row in matrix

--- medium_problems/k_weakest_row.js
+++ medium_problems/k_weakest_row.js
@@ -1,3 +1,12 @@
+// Given a m * n matrix mat of ones(representing soldiers) and 
+// zeros(representing civilians), return the indexes of the k weakest rows in 
+// the matrix ordered from the weakest to the strongest.
+
+// A row i is weaker than row j, if the number of soldiers in row i is less than 
+// the number of soldiers in row j, or they have the same number of soldiers but 
+// i is less than j.Soldiers are always stand in the frontier of a row, that is, 
+// always ones may appear first and then zeros.
+
 var kWeakestRows = function (mat, k) {
   mat = mat.map((row, i) => [row.reduce((acc, num) => acc += num), i]);
   mat.sort((num1, num2) => num1[0] === num2[0] ? num1[1] - num2[1] : num1[0] - num2[0]);
